bdfebe5bf34c84d9365f55c38ac4aee73d1a2616
aixoio <79969530+aixoio@users.noreply.github.com>
2023-09-14 20:09:56 +1000

    Add new types

--- site/src/assets/ts/types.ts
+++ site/src/assets/ts/types.ts
@@ -1,2 +1,9 @@
 
 export type Difficulty = "easy" | "medium" | "hard";
+export interface CellState {
+    isMine: boolean;
+    isRevealed: boolean;
+    isFlagged: boolean;
+    adjacentMines: number;
+};
+export type Board = CellState[][];
