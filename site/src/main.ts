e41eae30dcc05ddba268484509c41cb7780d173b
aixoio <79969530+aixoio@users.noreply.github.com>
2023-09-14 19:44:04 +1000

    Add scss

--- site/src/main.ts
+++ site/src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import "@/assets/scss/master.scss";
+
 const app = createApp(App)
 
 app.use(createPinia())
